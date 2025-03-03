da23f7113efefa19b67204204f72dc4c318a5d3d
ArunREDDY-hash <75643540+ArunREDDY-hash@users.noreply.github.com>
2025-03-03 17:35:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD Pipeline!"
+    return "Hello Siva, Flask App running on K8's using Harness!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
